9fe5c75609f1e6291060d5dac462d9d11fd9804b
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2021-05-20 02:38:50 +0530

    New Commit

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function addNewWEField(){
 }
 
 
+
 function addNewAQField(){
     let newNode = document.createElement('textarea');
     newNode.classList.add('form-control');
@@ -31,3 +32,73 @@ function addNewAQField(){
     aqOb.insertBefore(newNode, aqAddButtonOb);
 
 }
+
+//Generating CV
+
+function generateCV(){
+    //long method
+    let nameField = document.getElementById("nameField").value;
+    let nameT1= document.getElementById('nameT1');
+
+    nameT1.innerHTML = nameField;
+
+    //direct method
+
+    document.getElementById("nameT2").innerHTML = nameField;
+
+    //contact
+ 
+    document.getElementById('contactT').innerHTML = document.getElementById('contactField').value;
+
+    //adddress
+    document.getElementById('addressT').innerHTML = document.getElementById('addressField').value;
+    
+    document.getElementById('fbT').innerHTML = document.getElementById('fbField').value;
+    
+    document.getElementById('linkedT').innerHTML = document.getElementById('linkedinField').value;
+    
+    document.getElementById('githubT').innerHTML = document.getElementById('githubeiField').value;
+
+    //objective
+
+    document.getElementById('objectiveT').innerHTML = document.getElementById('objectiveField').value;
+    
+
+    //work experience
+
+    let wes = document.getElementsByClassName('weField');
+
+    let str = '';
+
+    for(let e of wes)
+    {
+        str =  str + '<li>' +e.value+ '</li>'
+    }
+
+    document.getElementById('weT').innerHTML = str;
+
+
+//academic qualifications
+
+let aqs = document.getElementsByClassName('aqField');
+
+let str1= "";
+
+for(let e of aqs){
+    str1 = str1 + '<li>' + e.value + '</li>';
+}
+
+document.getElementById('aqT').innerHTML = str1;
+
+document.getElementById('cv-form').style.display='none';
+document.getElementById('cv-template').style.display='block';
+
+    
+}
+//print CV
+
+function printCV(){
+    window.print();
+
+}
+
